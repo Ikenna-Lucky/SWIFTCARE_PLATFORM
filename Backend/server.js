e556561872448ffa57bbdf264efccbd3a7ffcbac
Ikenna Mbaekwe <goodluckikenna215@gmail.com>
2025-04-04 00:31:43 +0100

    Added my frontend urls to my Backend

--- Backend/server.js
+++ Backend/server.js
@@ -13,9 +13,11 @@ const port = process.env.PORT || 4000;
 connectDB();
 connectCloudinary();
 
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 //api endpoints
 app.use("/api/admin", adminRouter);
